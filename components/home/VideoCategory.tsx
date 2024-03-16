58a87f201a75539202a8e076492a2da600d36913
MohammadRohani-8483 <mohammad.rohani.1002@gmail.com>
2024-03-16 15:22:08 +0330

    Change the design to get closer to the user interface

--- components/home/VideoCategory.tsx
+++ components/home/VideoCategory.tsx
@@ -1,59 +1,44 @@
-import { videoCategory } from "@/public/data/videoCategory"
-import { FaChevronLeft } from "react-icons/fa";
-import Link from "@/node_modules/next/link";
+import VideoItem from "./VideoItem";
 
 export default function VideoCategory() {
-
     return (
-        <div className="grid lg:grid-cols-4 grid-cols-2 gap-2 lg:gap-4 max-w-[1136px] w-[90%] mx-auto mt-20 justify-items-center">
-            {videoCategory.map((product, index) => (
-                index % 2 === 0 ?
-                    <div key={product.id} className="lg:gap-4 gap-2 flex flex-col">
-                        <video autoPlay loop
-                            className='object-cover max-w-[272px] w-full h-[431px] rounded-3xl'
-                            itemType="video/mp4"
-                            src={product.video}
-                        />
-                        <div className={`rounded-3xl max-w-[272px] w-full lg:h-[221px] h-[200px] text-${product.color}-300 bg-${product.color}-200 p-4 gap-4 flex flex-col items-center justify-center text-center`}>
-                            <h1 className="lg:text-2xl text-base font-bold">{product.name}</h1>
-                            <p className={`lg:text-lg text-sm`}>
-                                {product.description}
-                            </p>
-                            <button className={`rounded-lg text-white bg-${product.color}-300 text-sm`}>
-                                <Link
-                                    className="flex justify-center items-center gap-2 hover:backdrop-brightness-[0.8] w-full h-full px-3 py-1.5 rounded-lg"
-                                    href={product.link}
-                                >
-                                    خرید
-                                    <FaChevronLeft />
-                                </Link>
-                            </button>
-                        </div>
-                    </div>
-                    :
-                    <div key={product.id} className="gap-4 flex flex-col">
-                        <div className={`rounded-3xl max-w-[272px] w-full lg:h-[221px] h-[200px] text-${product.color}-300 bg-${product.color}-200 p-4 gap-4 flex flex-col items-center justify-center text-center`}>
-                            <h1 className="lg:text-2xl text-base font-bold">{product.name}</h1>
-                            <p className={`lg:text-lg text-sm`}>
-                                {product.description}
-                            </p>
-                            <button className={`rounded-lg text-white bg-${product.color}-300 text-sm`}>
-                                <Link
-                                    className="flex justify-center items-center gap-2 hover:backdrop-brightness-[0.8] w-full h-full px-3 py-1.5 rounded-lg"
-                                    href={product.link}
-                                >
-                                    خرید
-                                    <FaChevronLeft />
-                                </Link>
-                            </button>
-                        </div>
-                        <video autoPlay loop
-                            className='object-cover max-w-[272px] w-full h-[431px] rounded-3xl'
-                            itemType="video/mp4"
-                            src={product.video}
-                        />
-                    </div>
-            ))}
+        <div className="flex justify-center items-center gap-2 md:gap-4 max-w-[1136px] w-[90%] mx-auto mt-20 ltr">
+            <div className="flex flex-col md:flex-row gap-2 md:gap-4 justify-center items-center ltr">
+                <VideoItem bgColor="#F5EEE7" color="#DB7A18"
+                    link="/products/کره-بادام-درختی" firstDiv
+                    title="کره بادام درختی"
+                    desc="تهیه شده از بادام ممتاز ایرانی"
+                    video="/video/kare-badam.mp4"
+                    classNameDiv="aspect-[140/152] md:aspect-[272/218]"
+                    classNameVideo="aspect-[140/168.7] md:aspect-[272/347]"
+                />
+                <VideoItem bgColor="#FAEEED" color="#BD483E"
+                    link="/products/روغن-کنجد-فرابکر-ممتاز"
+                    title="روغن کنجد فرابکر"
+                    desc="تهیه روز با پرس سرد فقط با تصفیه فیزیکی"
+                    video="/video/roghan-konjed.mp4"
+                    classNameDiv="aspect-[140/224] md:aspect-[272/347]"
+                    classNameVideo="aspect-[140/175.5] md:aspect-[272/361]"
+                />
+            </div>
+            <div className="flex flex-col md:flex-row gap-2 md:gap-4 justify-center items-center ltr">
+                <VideoItem bgColor="#E5EDFF" color="#5375C2"
+                    link="/products/روغن-سالاد" firstDiv
+                    title="روغن مخصوص سالاد"
+                    desc="حاوی قوی ترین آنتی اکسیدان های شناخته شده در دنیا"
+                    video="/video/roghan-salad.mp4"
+                    classNameDiv="aspect-[140/264] md:aspect-[272/221]"
+                    classNameVideo="aspect-[140/176.1] md:aspect-[272/362.3]"
+                />
+                <VideoItem bgColor="#E7F2F5" color="#3D8361"
+                    link="/products/ارده-کنجد"
+                    title="ارده کنجد"
+                    desc="تهیه شده به روش سنتی از کنجد ایرانی"
+                    video="/video/arde-konjed.mp4"
+                    classNameDiv="aspect-[140/183] md:aspect-[272/221]"
+                    classNameVideo="aspect-[140/209.5] md:aspect-[272/431]"
+                />
+            </div>
         </div>
     )
 }
---
+++ components/home/VideoItem.tsx
@@ -0,0 +1,52 @@
+import Link from 'next/link'
+import React from 'react'
+import { FaChevronLeft } from 'react-icons/fa'
+
+type props = {
+    video: string
+    bgColor: string
+    color: string
+    title: string
+    desc: string
+    classNameVideo: string
+    classNameDiv: string
+    link: string
+    firstDiv?: boolean
+}
+
+export default function VideoItem({ video, bgColor, color, title, desc, classNameVideo, classNameDiv, link, firstDiv }: props) {
+    return (
+        <div
+            className={`gap-4 flex flex-col ${firstDiv ? "md:flex-col-reverse" : ""}`}
+        >
+            <video autoPlay loop
+                className={`object-cover max-w-[272px] w-full h-auto rounded-3xl ${classNameVideo}`}
+                itemType="video/mp4"
+                src={video}
+            />
+            <div
+                style={{ color: color, backgroundColor: bgColor }}
+                className={`rounded-3xl max-w-[272px] w-full h-auto p-4 gap-4 flex flex-col items-center justify-center text-center ${classNameDiv}`}
+            >
+                <h1 className="lg:text-2xl text-base font-bold">
+                    {title}
+                </h1>
+                <p className={`lg:text-lg text-sm`}>
+                    {desc}
+                </p>
+                <Link
+                    href={link}
+                >
+                    <button
+                        style={{ backgroundColor: color }}
+                        className='rounded-lg text-white text-sm flex justify-center items-center gap-2 hover:backdrop-brightness-[0.8] px-3 py-1.5 '
+                    >
+                        خرید
+                        <FaChevronLeft />
+                    </button>
+                </Link>
+            </div>
+        </div>
+    )
+}
+
