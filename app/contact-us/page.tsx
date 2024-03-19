9763922920e515711daba22cc37a046295c395b9
MohammadRohani-8483 <mohammad.rohani.1002@gmail.com>
2024-03-19 13:24:34 +0330

    add background vector

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -1,16 +1,33 @@
 import Communications from '@/components/contact-us/communications/Communications'
 import SendMessageToPedarbozorg from '@/components/contact-us/SendMessageToPedarbozorg'
 import Title from '@/components/Title'
+import Image from 'next/image'
 import React from 'react'
 
 export default function page() {
     return (
-        <main className='w-[90%] mx-auto max-w-[1136px] flex flex-col justify-center items-center gap-16 py-20 md:py-[117px]'>
-            <Title>تماس با ما</Title>
-            <div className='flex flex-col w-full justify-center items-center gap-2 lg:gap-6'>
-                <SendMessageToPedarbozorg />
-                <Communications />
-            </div>
-        </main>
+        <>
+            <Image
+                src="/Image/background/vectors/tree-2.svg"
+                alt="tree 2"
+                width={219}
+                height={218}
+                className='top-2 left-0 absolute hidden md:block z-[-1]'
+            />
+            <Image
+                src="/Image/background/vectors/tree-1.svg"
+                alt="tree 1"
+                width={152}
+                height={192}
+                className='top-12 right-0 absolute hidden md:block z-[-1]'
+            />
+            <main className='w-[90%] mx-auto max-w-[1136px] flex flex-col justify-center items-center gap-16 py-20 md:py-[117px]'>
+                <Title>تماس با ما</Title>
+                <div className='flex flex-col w-full justify-center items-center gap-2 lg:gap-6'>
+                    <SendMessageToPedarbozorg />
+                    <Communications />
+                </div>
+            </main>
+        </>
     )
 }
