4844832d9415683185f21eee9bac81db429b9648
MohammadRohani-8483 <mohammad.rohani.1002@gmail.com>
2024-03-23 20:32:11 +0330

    add Alert component in account component in profile page

---
+++ components/Alert.tsx
@@ -0,0 +1,104 @@
+import React, { ReactNode, useEffect, useState } from 'react'
+import { motion } from "framer-motion";
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import Image from 'next/image'
+import { IoIosClose } from "react-icons/io";
+import toast, { Toaster } from 'react-hot-toast';
+
+type props = {
+    title: string
+    setIsAlertOpen: (par: boolean) => void
+    confirmFunc: () => void
+    redBtn?: boolean
+    messageToast: string
+    children: ReactNode
+    textBtn: string
+}
+
+const Alert = ({title, confirmFunc, redBtn, setIsAlertOpen, messageToast, children, textBtn }: props) => {
+    const [isAnimate, setIsAnimate] = useState(true)
+
+    const variants = {
+        visible: {
+            y: 0,
+            opacity: 1
+        },
+        hidden: {
+            y: "50vh",
+            opacity: 0
+        },
+        bgHidden: {
+            opacity: 0
+        },
+        bgVisible: {
+            opacity: 1
+        },
+    };
+
+    useEffect(() => {
+        document.documentElement.classList.add('overflow-hidden')
+
+        return () => {
+            document.documentElement.classList.remove('overflow-hidden')
+        }
+    }, [])
+
+
+    const handleClose = () => {
+        setIsAnimate(false)
+        document.documentElement.classList.remove('overflow-hidden')
+        setTimeout(() => {
+            setIsAlertOpen(false)
+        }, 300)
+    }
+
+    const handleConfirm = () => {
+        confirmFunc()
+        handleClose()
+        toast.success(messageToast)
+    }
+
+    return (
+            <motion.div
+                onClick={(e: any) => e.target.className === "fixed inset-0 bg-black bg-opacity-50 w-screen h-screen z-[2000] flex justify-center items-center" && handleClose()}
+                animate={isAnimate ? "bgVisible" : "bgHidden"}
+                initial={isAnimate ? "bgHidden" : "bgVisible"}
+                variants={variants}
+                className='fixed inset-0 bg-black bg-opacity-50 w-screen h-screen z-50 flex justify-center items-center'
+            >
+                <motion.div
+                
+                    className='flex flex-col justify-center items-center p-4 md:p-6 gap-4 md:gap-6 bg-white rounded-3xl w-[288px] md:w-auto'
+                    animate={isAnimate ? "visible" : "hidden"}
+                    initial={isAnimate ? "hidden" : "visible"}
+                    variants={variants}
+                    transition={{ duration: 0.4 }}
+                >
+                    <div className='flex justify-between items-center text-base-300 text-xl font-bold w-full'>
+                        <h1>
+                            {title}
+                        </h1>
+                        <IoIosClose className='text-3xl cursor-pointer'
+                            onClick={handleClose} />
+                    </div>
+                    {children}
+                    <div className='flex w-full justify-end gap-3 md:gap-4'>
+                        <button
+                            onClick={handleClose}
+                            className='rectangle-btn outline-btn !border-red-600 !bg-white !text-red-600 hover:!bg-red-50'
+                        >
+                            انصراف
+                        </button>
+                        <button
+                            onClick={handleConfirm}
+                            className={`rectangle-btn solid-btn ${redBtn ? "!bg-red-600 hover:!bg-red-700" : ""}`}
+                        >
+                            {textBtn}
+                        </button>
+                    </div>
+                </motion.div>
+            </motion.div>
+    )
+}
+
+export default Alert
--- components/profile/Account.tsx
+++ components/profile/Account.tsx
@@ -1,8 +1,19 @@
+'use client'
 import Image from 'next/image'
-import React from 'react'
+import { useRouter } from 'next/navigation'
+import React, { useState } from 'react'
+import Alert from '../Alert'
 import ProfileBox from './ProfileBox'
 
 const Account = () => {
+    const [isLogingOut, setIsLogingOut] = useState(false)
+
+    const { replace } = useRouter()
+
+    const handleLogOut = () => {
+        replace('/')
+    }
+
     return (
         <ProfileBox>
             <div className='w-full flex flex-col gap-2 md:gap-4 justify-center items-center'>
@@ -17,85 +28,115 @@ const Account = () => {
                     </div>
                     حساب من در یک نگاه
                 </div>
-                <div className='w-full flex justify-start items-center text-sm md:text-xl text-base-300'>
-                    <div className="-mr-2 size-10 flex justify-center items-center" >
-                        <Image
-                            src='/iconSax/box.svg'
-                            alt="orders"
-                            width={24}
-                            height={24}
-                        />
+                <div className='w-full flex justify-start items-center'>
+                    <div className='cursor-pointer flex justify-center items-center text-sm md:text-xl text-base-300'>
+                        <div className="-mr-2 size-10 flex justify-center items-center" >
+                            <Image
+                                src='/iconSax/box.svg'
+                                alt="orders"
+                                width={24}
+                                height={24}
+                            />
+                        </div>
+                        سفارش ها
                     </div>
-                    سفارش ها
                 </div>
-                <div className='w-full flex justify-start items-center text-sm md:text-xl text-base-300'>
-                    <div className="-mr-2 size-10 flex justify-center items-center" >
-                        <Image
-                            src='/iconSax/heart.svg'
-                            alt="my favorite list"
-                            width={24}
-                            height={24}
-                        />
+                <div className='w-full flex justify-start items-center'>
+                    <div className='cursor-pointer flex justify-center items-center text-sm md:text-xl text-base-300'>
+                        <div className="-mr-2 size-10 flex justify-center items-center" >
+                            <Image
+                                src='/iconSax/heart.svg'
+                                alt="my favorite list"
+                                width={24}
+                                height={24}
+                            />
+                        </div>
+                        لیست علافه مندی من
                     </div>
-                    لیست علافه مندی من
                 </div>
-                <div className='w-full flex justify-start items-center text-sm md:text-xl text-base-300'>
-                    <div className="-mr-2 size-10 flex justify-center items-center" >
-                        <Image
-                            src='/iconSax/bg-location.svg'
-                            alt="locations"
-                            width={24}
-                            height={24}
-                        />
+                <div className='w-full flex justify-start items-center'>
+                    <div className='cursor-pointer flex justify-center items-center text-sm md:text-xl text-base-300'>
+                        <div className="-mr-2 size-10 flex justify-center items-center" >
+                            <Image
+                                src='/iconSax/bg-location.svg'
+                                alt="locations"
+                                width={24}
+                                height={24}
+                            />
+                        </div>
+                        آدرس ها
                     </div>
-                    آدرس ها
                 </div>
-                <div className='w-full flex justify-start items-center text-sm md:text-xl text-base-300'>
+                <div className='w-full flex justify-start items-center'>
                     <div className="-mr-2 size-10 flex justify-center items-center" >
-                        <Image
-                            src='/iconSax/mail.svg'
-                            alt="messages"
-                            width={24}
-                            height={24}
-                        />
+                        <div className='cursor-pointer flex justify-center items-center text-sm md:text-xl text-base-300'>
+                            <Image
+                                src='/iconSax/mail.svg'
+                                alt="messages"
+                                width={24}
+                                height={24}
+                            />
+                        </div>
+                        پیغام ها
                     </div>
-                    پیغام ها
                 </div>
-                <div className='w-full flex justify-start items-center text-sm md:text-xl text-base-300'>
-                    <div className="-mr-2 size-10 flex justify-center items-center" >
-                        <Image
-                            src='/iconSax/message-text.svg'
-                            alt="comments"
-                            width={24}
-                            height={24}
-                        />
+                <div className='w-full flex justify-start items-center'>
+                    <div className='cursor-pointer flex justify-center items-center text-sm md:text-xl text-base-300'>
+                        <div className="-mr-2 size-10 flex justify-center items-center" >
+                            <Image
+                                src='/iconSax/message-text.svg'
+                                alt="comments"
+                                width={24}
+                                height={24}
+                            />
+                        </div>
+                        نظرات
                     </div>
-                    نظرات
                 </div>
-                <div className='w-full flex justify-start items-center text-sm md:text-xl text-base-300'>
-                    <div className="-mr-2 size-10 flex justify-center items-center" >
-                        <Image
-                            src='/iconSax/user-square.svg'
-                            alt="user account"
-                            width={24}
-                            height={24}
-                        />
+                <div className='w-full flex justify-start items-center'>
+                    <div className='cursor-pointer flex justify-center items-center text-sm md:text-xl text-base-300'>
+                        <div className="-mr-2 size-10 flex justify-center items-center" >
+                            <Image
+                                src='/iconSax/user-square.svg'
+                                alt="user account"
+                                width={24}
+                                height={24}
+                            />
+                        </div>
+                        مشخصات حساب کاربری
                     </div>
-                    مشخصات حساب کاربری
                 </div>
-                <div className='w-full flex justify-start items-center text-sm md:text-xl text-[#C62020]'>
-                    <div className="-mr-2 size-10 flex justify-center items-center" >
-                        <Image
-                            src='/iconSax/logout.svg'
-                            alt="logout"
-                            width={24}
-                            height={24}
-                        />
+                <div className='w-full flex justify-start items-center'>
+                    <div
+                        onClick={() => setIsLogingOut(true)}
+                        className='cursor-pointer flex justify-center items-center text-sm md:text-xl text-[#C62020]'
+                    >
+                        <div className="-mr-2 size-10 flex justify-center items-center" >
+                            <Image
+                                src='/iconSax/logout.svg'
+                                alt="logout"
+                                width={24}
+                                height={24}
+                            />
+                        </div>
+                        خروج از حساب
                     </div>
-                    خروج از حساب
+                    {isLogingOut &&
+                        <Alert
+                            confirmFunc={handleLogOut}
+                            messageToast='با موفقیت از حساب خود خارج شدید'
+                            setIsAlertOpen={setIsLogingOut}
+                            textBtn='خروج از حساب'
+                            title='خروج از حساب کاربری' redBtn
+                        >
+                            <p className='text-[#353535] w-full text-right'>
+                                برای سفارش و مشاهده سبد خرید بایستی وارد حساب خود باشید
+                            </p>
+                        </Alert>
+                    }
                 </div>
             </div>
-        </ProfileBox>
+        </ProfileBox >
     )
 }
 
