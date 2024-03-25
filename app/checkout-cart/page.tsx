454ff7edea4fb5308269fe23eb2a58a88ef8e5de
MohammadRohani-8483 <mohammad.rohani.1002@gmail.com>
2024-03-26 00:59:54 +0330

    change checkoute page address

--- app/checkout-cart/page.tsx
+++ app/checkout-cart/page.tsx
@@ -12,6 +12,14 @@ import Loading from './loading'
 const CheckoutCart = () => {
     const cart: shopCartItem[] = useSelector((state: any) => state.cart.cart)
 
+    const [totalFinalPrice, setTotalFinalPrice] = useState(0)
+    const [totalSellPrice, setTotalSellPrice] = useState(0)
+
+    useEffect(() => {
+        setTotalFinalPrice(cart.reduce((previous, current) => previous + current?.shatootInfo.finalPrice * current?.quantity, 0))
+        setTotalSellPrice(cart.reduce((previous, current) => previous + current?.shatootInfo.sellPrice * current?.quantity, 0))
+    }, [cart])
+
     const [start, setStart] = useState(false)
     useEffect(() => {
         setStart(true)
@@ -47,7 +55,16 @@ const CheckoutCart = () => {
                             ?
                             <div className='flex flex-col md:flex-row w-full justify-center items-start gap-2 lg:gap-6'>
                                 <CheckoutItems />
-                                <SubmitOrderBox />
+                                <SubmitOrderBox
+                                    page='CART'
+                                    countProduct={cart?.length}
+                                    finalPrice={totalFinalPrice}
+                                    sellPrice={totalSellPrice}
+                                    discountProducts={totalSellPrice - totalFinalPrice}
+                                    discountProductsPercent={(totalSellPrice - totalFinalPrice) / totalSellPrice * 100}
+                                    yourProfit={totalSellPrice - totalFinalPrice}
+                                    yourProfitPercent={(totalSellPrice - totalFinalPrice) / totalSellPrice * 100}
+                                />
                             </div>
                             :
                             <EmptyCheckoutCart />
