3f327e6036c14f295eb6c453e825a03f9ca4af42
MohammadRohani-8483 <mohammad.rohani.1002@gmail.com>
2024-03-26 01:02:57 +0330

    create address type and fake address for test codes

---
+++ public/types/adress.ts
@@ -0,0 +1,30 @@
+export type address = {
+    id: number
+    first_name: string
+    last_name: string
+    province: number | string
+    city: number | string
+    strict?: string
+    unit_no: number
+    flat_no: number
+    zip_code: string
+    address: string
+    phone_number: string
+    is_active: boolean
+    alley: number
+}
+
+export const fakeAddress: address = {
+    id: 2325,
+    province: "قم",
+    city: "قم",
+    address: 'بلوار امین',
+    alley: 15,
+    flat_no: 2,
+    unit_no: 5,
+    zip_code: "3713526895",
+    phone_number: "09330146331",
+    first_name: "محمد",
+    last_name: "روحانی",
+    is_active: true,
+}
