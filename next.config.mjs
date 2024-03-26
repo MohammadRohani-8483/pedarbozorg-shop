57e69d97d23b1ed6f948a7e50ada6e613dcdc0ad
MohammadRohani-8483 <mohammad.rohani.1002@gmail.com>
2024-03-27 00:10:46 +0330

    change API base url

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,13 @@ const API_URL = process.env.API;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["trustseal.enamad.ir", "api.pedarbozorg.shop"],
+    domains: ["pedarbozorg-api.codintofuture.ir"],
   },
   trailingSlash: true,
   rewrites: async () => [
     {
       source: "/api/:path*",
-      destination: `https://api.pedarbozorg.shop/:path*/`,
+      destination: `https://pedarbozorg-api.codintofuture.ir/:path*/`,
       basePath: false,
     },
   ],
