cd13221dd489ffb4006b51f63b4a1ac9f5d800df
MohammadRohani-8483 <mohammad.rohani.1002@gmail.com>
2024-04-01 13:43:17 +0330

    create layout component for profile page

---
+++ app/profile/layout.tsx
@@ -0,0 +1,39 @@
+'use client'
+import Account from '@/components/profile/Account'
+import Info from '@/components/profile/Info'
+import { authState } from '@/public/redux/store/auth'
+import { useRouter } from 'next/navigation'
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import Loading from './loading'
+
+const LayoutProfile = ({ children }: any) => {
+    const isLogedIn = useSelector((state: { auth: authState }) => state.auth.isLogedIn)
+    const { replace } = useRouter()
+    const [start, setStart] = useState(false)
+
+    useEffect(() => {
+        !isLogedIn &&
+            replace('/')
+        setStart(true)
+    }, [])
+    return (
+        <>
+            {start ?
+                <main className='grid grid-cols-1 md:grid-cols-3 justify-items-center justify-center w-[90%] max-w-[1136px] mx-auto gap-4 py-24 md:py-36'>
+                    <div className='w-full flex flex-col gap-4'>
+                        <Info />
+                        <Account />
+                    </div>
+                    <div className='md:col-start-2 md:col-end-4 w-full flex flex-col gap-4'>
+                        {children}
+                    </div>
+                </main>
+                :
+                <Loading />
+            }
+        </>
+    )
+}
+
+export default LayoutProfile
