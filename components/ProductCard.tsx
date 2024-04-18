f63764f5fcbed78738797031096aad4792ac4905
MohammadRohani-8483 <mohammad.rohani.1002@gmail.com>
2024-04-18 16:36:15 +0330

    Change how to insert a photo

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -4,7 +4,6 @@ import Link from '@/node_modules/next/link';
 import Image from '@/node_modules/next/image';
 import { FiCameraOff } from "@/node_modules/react-icons/fi/index";
 import { FaStar } from "@/node_modules/react-icons/fa/index";
-import tooman from "@/public/Image/tooman.svg";
 import formatNumber from '@/public/Functions/formatNumber';
 import { motion } from 'framer-motion';
 import { useDispatch, useSelector } from 'react-redux';
@@ -188,7 +187,7 @@ const ProductCard = ({ price, link, image, name, priceWithOffer, score, product 
                                     </p>
                                     {priceWithOffer !== 0 &&
                                         <Image
-                                            src={tooman}
+                                            src='/Image/tooman.svg'
                                             alt='تومان'
                                             width={23}
                                             height={16}
